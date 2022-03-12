d189a5ff85d90a450edc086dc1c50fac24a79c75
arsh <36216433+ArshNoman@users.noreply.github.com>
2022-03-12 21:32:58 +0800

    Added translations and comments

--- -e-+/ui.py
+++ -e-+/ui.py
@@ -17,6 +17,8 @@ import glob
 
 username = ''; password = ''; logged_in = False; session_user = ''
 
+# 连接到 MySQL 数据库
+
 db = pymysql.connect(
     host="byyikg99odchbcly6vrw-mysql.services.clever-cloud.com",
     user="ubyvkstgmoy2hv2b",
@@ -30,25 +32,25 @@ db.ping()  # reconnecting mysql
 if cursor.connection is None:
     db.ping()
 
-# Login initialize
-
+# 登录功能
 def main_login():
+
+    # 创建APP窗口
+
     login_root = Tk()
     login_root.geometry('375x150')
     login_root.title("Login")
     login_root.resizable(False, False)
-    # login_root.iconbitmap("C:/Users/PC/Desktop/7/logo.ico")
 
     usernamelabel = Label(login_root, text="Username | 用户名", relief=RIDGE)
     emptyrow = Label(login_root)
     emptycolumn = Label(login_root)
-    emptycolumn2 = Label(login_root)
     emptyrow2 = Label(login_root)
     passwordlabel = Label(login_root, text="Password | 密码", relief=RIDGE)
     usernameentry = Entry(login_root, relief=GROOVE)
     passwordentry = Entry(login_root, relief=GROOVE, show='*')
 
-    # Functions
+    # 职能
 
     def register():
         def register_user():
@@ -78,11 +80,12 @@ def main_login():
             welcome_reg.pack()
             register_screen.destroy()
 
+        # 创建新的APP窗口
+
         register_screen = Toplevel(login_root)
         register_screen.title("Register | 登记")
         register_screen.geometry("250x220")
         register_screen.resizable(False, False)
-        # register_screen.iconbitmap("C:/Users/PC/Desktop/7/logo.ico")
 
         username_reg = Label(register_screen, text="Username").pack()
         username_entry_register = Entry(register_screen)
@@ -111,7 +114,7 @@ def main_login():
             messagebox.showerror('Empty space found', 'All fields are required to be filled | 所有字段都必须填写')
 
         try:
-            cursor.execute("SELECT password FROM users WHERE username = (%s)", username_given)
+            cursor.execute("SELECT password FROM users WHERE username = (%s)", username_given) # 访问数据库
             passw = cursor.fetchall()[0][0]
 
             if passw == password_given:
@@ -139,12 +142,13 @@ def main_login():
     def app():
         global session_user
 
+        # 创建APP窗口
+
         root = tk.ThemedTk()
         root.get_themes()
         root.set_theme('scidgrey')
         root.title('Project')
         root.geometry('1000x750')
-        # root.iconbitmap("C:/Users/PC/Desktop/7/logo.ico")
         root.resizable(False, False)
 
         image = PhotoImage(file='static/bg.png')
@@ -153,8 +157,10 @@ def main_login():
 
         def view_violations():
 
+            # 创建APP窗口
+
             violation_root = Toplevel(root)
-            violation_root.title("View Violations")
+            violation_root.title("View Violations | 查看违规行为")
             violation_root.geometry("750x500")
             violation_root.resizable(False, False)
 
@@ -175,7 +181,9 @@ def main_login():
                 files = glob.glob('pics/*')
                 for f in files:
                     os.remove(f)
-                
+
+                # 从数据库中访问违规图像并将其保存在本地
+
                 if violations != []:
                     for i in violations:
                         with open(f'pics/img{index}.png', 'wb') as file:
@@ -190,7 +198,7 @@ def main_login():
                         images.append(ImageTk.PhotoImage(Image.open(f'pics/img{x}.png')))
 
                     viewing = Toplevel(root)
-                    viewing.title("Violations")
+                    viewing.title("Violations | 违规行为")
                     viewing.geometry("750x500")
                     viewing.resizable(False, False)
 
@@ -199,7 +207,7 @@ def main_login():
                     label.grid(row=0, column=0, columnspan=3)
                     status.grid(row=3, column=1)
 
-                    def next(img_num):
+                    def next(img_num): # 移动到下一张图片的功能
                         global label
                         global button_forward
                         global button_back
@@ -219,7 +227,7 @@ def main_login():
                         button_back.grid(row=1, column=0)
                         button_forward.grid(row=1, column=2)
 
-                    def back(img_num):
+                    def back(img_num): # 移动到上一张图像的功能
                         global label
                         global button_forward
                         global button_back
@@ -241,7 +249,7 @@ def main_login():
                         button_forward.grid(row=1, column=2)
 
                     button_back = Button(viewing, text='<<', command=lambda: back())
-                    button_quit = Button(viewing, text='Exit Program', command=viewing.quit)
+                    button_quit = Button(viewing, text='Exit | 出口', command=viewing.destroy())
                     button_forward = Button(viewing, text='>>', command=lambda: next(2))
 
                     button_back.grid(row=1, column=0)
@@ -255,8 +263,8 @@ def main_login():
 
             listbox = Listbox(violation_root)
             empty = Label(violation_root)
-            viewv = Button(violation_root, text='View Violations', width=15, command=viewclass)
-            select = Label(violation_root, text='Select a class')
+            viewv = Button(violation_root, text='View Violations | 查看违规行为', width=15, command=viewclass)
+            select = Label(violation_root, text='Select a class | 选课')
             listbox.config(width=100)
 
             db.ping()
@@ -272,7 +280,7 @@ def main_login():
                         listbox.grid(row=0, column=1)
                         index -= 1
                 else:
-                    listbox.insert(0, 'No Classes Available')
+                    listbox.insert(0, 'No Classes Available | 没有课程')
                     listbox.grid(row=0, column=1)
             except:
                 pass
@@ -282,13 +290,15 @@ def main_login():
 
         def manage_classes():
 
+                # 创建APP窗口
+
                 classes = Toplevel(root)
                 classes.title("Class Management")
                 classes.geometry("750x500")
                 # open_files.iconbitmap("C:/Users/PC/Desktop/7/logo.ico")
                 classes.resizable(False, False)
 
-                def removeclass():
+                def removeclass(): # 从数据库中删除一个类
                     db.ping()
                     cursor.execute("SELECT classes FROM users WHERE username = (%s)", (session_user,))
                     clist = ast.literal_eval(cursor.fetchall()[0][0])
@@ -310,10 +320,10 @@ def main_login():
 
                     classes.destroy()
 
-                def addclass():
+                def addclass(): # 向数据库中添加一个类
 
                     new_folder = Toplevel(root)
-                    new_folder.title("Add a class")
+                    new_folder.title("Add a class | 添加一个类")
                     new_folder.geometry("320x220")
                     new_folder.resizable(False, False)
 
@@ -322,7 +332,7 @@ def main_login():
                     def adddclass():
 
                         if title.get() == '':
-                            messagebox.showerror('Name Empty', 'Your class name cannot be empty!')
+                            messagebox.showerror('Name Empty', 'Your class name cannot be empty! | 类名不能为空！')
                         else:
                             db.ping()
 
@@ -340,11 +350,11 @@ def main_login():
                         classes.destroy()
 
                     title = ttk.Entry(new_folder, width=48)
-                    confirm_create = Button(new_folder, text='Add new class', command=adddclass)
+                    confirm_create = Button(new_folder, text='Add new class | 添加新班级', command=adddclass)
 
                     Label(new_folder).grid(row=0, column=0)
                     Label(new_folder).grid(row=0, column=1)
-                    Label(new_folder, text='Enter class name').grid(row=1, column=2)
+                    Label(new_folder, text='Enter class name | 输入班级名称').grid(row=1, column=2)
                     Label(new_folder).grid(row=2, column=1)
                     title.grid(row=3, column=2)
                     Label(new_folder).grid(row=4, column=1)
@@ -354,9 +364,9 @@ def main_login():
 
                 listbox = Listbox(classes)
                 empty = Label(classes)
-                add_classes = Button(classes, text='Add a class', width=15, command=addclass)
-                remove_classes = Button(classes, text='Remove class', width=15, command=removeclass)
-                your_classes_label = Label(classes, text='Your classes')
+                add_classes = Button(classes, text='Add a class | 添加新班级', width=15, command=addclass)
+                remove_classes = Button(classes, text='Remove class | 删除类', width=15, command=removeclass)
+                your_classes_label = Label(classes, text='Your classes | 你的课')
                 listbox.config(width=100)
 
                 db.ping()
@@ -372,7 +382,7 @@ def main_login():
                             listbox.grid(row=0, column=1)
                             index -= 1
                     else:
-                        listbox.insert(0, 'No Classes Available')
+                        listbox.insert(0, 'No Classes Available | 没有课程')
                         listbox.grid(row=0, column=1)
                 except: pass
 
@@ -389,16 +399,16 @@ def main_login():
                 root.destroy()
                 main_login()
 
-        view = ttk.Button(root, text='View Violations', command=view_violations, width=20)
-        manage_classes = ttk.Button(root, text='Manage classes', command=manage_classes, width=20)
+        view = ttk.Button(root, text='View Violations | 查看违规行为', command=view_violations, width=20)
+        manage_classes = ttk.Button(root, text='Manage classes | 管理课程', command=manage_classes, width=20)
 
         menu_app = Menu(root)
         root.config(menu=menu_app)
         submenu_app = Menu(menu_app, tearoff=0)
 
-        menu_app.add_cascade(label='Settings', menu=submenu_app)
-        submenu_app.add_command(label='Contact', command=contact)
-        submenu_app.add_command(label='Log out', command=log_out)
+        menu_app.add_cascade(label='Settings | 设置', menu=submenu_app)
+        submenu_app.add_command(label='Contact | 接触', command=contact)
+        submenu_app.add_command(label='Log out | 登出', command=log_out)
 
         Label(root).grid(row=0, column=0)
         view.grid(row=1, column=1)
